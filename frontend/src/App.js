1ce1106410fffdcb49f899e6fcd35284c00d7d49
Pranav Harlalka <pranavharlalka2@gmail.com>
2025-10-22 01:52:00 +0530

    Register page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./common/Navbar";
 import Footer from "./common/Footer";
+import Register from "./pages/Register";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path="/register" element={<Register/>} />
+      </Routes>
       <Footer />
     </BrowserRouter>
 
